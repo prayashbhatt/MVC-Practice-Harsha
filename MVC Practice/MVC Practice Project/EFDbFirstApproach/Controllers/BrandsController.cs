c5a4f36a7e554e9b7b4ee586e3d0bca5c01ad2eb
Prayash Bhatt <prayash_bhatt1990@yahoo.com>
2022-12-08 22:41:52 +0530

    Application Committed to DataBase First Approach

---
+++ MVC Practice/MVC Practice Project/EFDbFirstApproach/Controllers/BrandsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFDbFirstApproach.Models;
+
+namespace EFDbFirstApproach.Controllers
+{
+    public class BrandsController : Controller
+    {
+        // GET: Brands
+        public ActionResult Index()
+        {
+            EFDBFirstDatabaseEntities eFDBFirstDatabaseEntities = new EFDBFirstDatabaseEntities();
+            List<Brand> brands = eFDBFirstDatabaseEntities.Brands.ToList();
+            return View(brands);
+        }
+    }
+}
---
+++ MVC Practice/MVC Practice Project/EFDbFirstApproach/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFDbFirstApproach.Models;
+
+namespace EFDbFirstApproach.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            EFDBFirstDatabaseEntities eFDBFirstDatabaseEntities = new EFDBFirstDatabaseEntities();
+            List<Category> categories = eFDBFirstDatabaseEntities.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
