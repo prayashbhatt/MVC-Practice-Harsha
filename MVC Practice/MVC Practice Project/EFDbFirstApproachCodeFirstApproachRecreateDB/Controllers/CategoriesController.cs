4c5153200527d07a1b8c66a8872043d4dbdbe442
Prayash Bhatt <prayash_bhatt1990@yahoo.com>
2022-12-11 15:23:50 +0530

    Commit for Automatic Migrations

---
+++ MVC Practice/MVC Practice Project/EFDbFirstApproachCodeFirstApproachRecreateDB/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using EFDbFirstApproachCodeFirstApproachRecreateDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFDbFirstApproachCodeFirstApproachRecreateDB.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            CompanyDbContext dbContext = new CompanyDbContext();
+            List<Category> categories = dbContext.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
