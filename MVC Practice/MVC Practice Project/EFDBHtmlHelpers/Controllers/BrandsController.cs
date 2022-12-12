6f826675c82772b630724e02c4b62f33a7d6e2cc
Prayash Bhatt <prayash_bhatt1990@yahoo.com>
2022-12-12 23:29:21 +0530

    Html Helpers commit

---
+++ MVC Practice/MVC Practice Project/EFDBHtmlHelpers/Controllers/BrandsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFDBHtmlHelpers.Models;
+
+namespace EFDBHtmlHelpers.Controllers
+{
+    public class BrandsController : Controller
+    {
+        // GET: Brands
+        public ActionResult Index()
+        {
+            CompanyDbContext dbContext = new CompanyDbContext();
+            List<Brand> brands = dbContext.Brands.ToList();
+            return View(brands);
+        }
+    }
+}
---
+++ MVC Practice/MVC Practice Project/EFDBHtmlHelpers/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using EFDBHtmlHelpers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFDBHtmlHelpers.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            CompanyDbContext dbContext = new CompanyDbContext();
+            List<Category> categories = dbContext.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
