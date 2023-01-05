1466e60a985516df4d091e47528a0245f558775b
Prayash Bhatt <prayash_bhatt1990@yahoo.com>
2023-01-05 22:45:22 +0530

    EFDBValidations added

---
+++ MVC Practice/MVC Practice Project/EFDBValidations/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EFDBValidations.MvcApplication" Language="C#" %>
---
+++ MVC Practice/MVC Practice Project/EFDBValidations/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EFDBHtmlHelpers.Models
+{
+    public class Category
+    {
+        [Key]
+        [Display(Name = "Category")]
+        public long CategoryID { get; set; }
+
+        [Display(Name = "Category Name")]
+        public string CategoryName { get; set; }
+    }
+}
